89326f2c50daf48cba7092bab89e632baf53cbc1
Raphael <raphael.bonadia@shockit.com.br>
2024-03-26 11:19:17 -0300

    projeto em flutter/dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,9 @@
+class Constants {
+  static const userFavoritesUrl = 'https://shop-app-45262-default-rtdb.firebaseio.com/userFavorites';
+
+  static const productBaseUrl = 'https://shop-app-45262-default-rtdb.firebaseio.com/products';
+
+  static const orderBaseUrl = 'https://shop-app-45262-default-rtdb.firebaseio.com/orders';
+
+  static const webApiKey = 'AIzaSyDVn8s7FFhvrAgSx2fEr-I-ic1tsfTOcc4';
+}
